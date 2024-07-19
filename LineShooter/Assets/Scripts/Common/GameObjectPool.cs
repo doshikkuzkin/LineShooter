1fd232f77d92e93f7b52ace06003942f0f4f2b52
doshikkuzkin <doshikkuzkin@gmail.com>
2024-07-19 09:11:07 +0300

    R! Clear usages

--- LineShooter/Assets/Scripts/Common/GameObjectPool.cs
+++ LineShooter/Assets/Scripts/Common/GameObjectPool.cs
@@ -1,7 +1,4 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameObjectPool<T> where T : UnityEngine.Component
--- LineShooter/Assets/Scripts/Common/RandomHelper.cs
+++ LineShooter/Assets/Scripts/Common/RandomHelper.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEditor;
 
 public static class RandomHelper
 {
